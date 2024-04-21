eb85120c9ee175fd0c9fd2c1f8790b085106dc26
Ahtesham Ansari <ahtesham6867@gmail.com>
2024-04-21 23:20:53 +0530

    hero section added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/HeroSection/Hero";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
